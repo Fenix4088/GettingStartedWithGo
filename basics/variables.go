cb219cb0a7fdf363e6ddbf466c2a8e5088adccda
Fenix4088 <fenix4088@gmail.com>
2022-09-06 22:53:37 +0300

    BMI practice

--- basics/variables.go
+++ basics/variables.go
@@ -3,6 +3,7 @@ package basics
 import (
 	"fmt"
 	"time"
+	"math"
 )
 
 type Bio struct {
@@ -15,4 +16,10 @@ func IntroduceYourself(bio Bio) {
 
 	fmt.Printf("Hello, my name is %v %v, I am %v y.o. and I was born in %v\n", bio.Name, bio.LastName, currentYear - bio.BirthYear, bio.Country)
 	
+}
+
+func CalcCircumference(radius float32) (float32, string) {
+	circumference := 2 * math.Pi * radius
+	
+	return circumference, fmt.Sprintf("For a radius of %v, the circle circumference is %.2f.", radius, circumference)
 }
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"gettingStartedWithGo/basics"
+	bmicalculator "gettingStartedWithGo/bmiCalculator"
 )
 
 func main() {
 	basics.IntroduceYourself(basics.Bio{"John", "Dou", "USA", 2000})
+
+	circ, explenation := basics.CalcCircumference(22)
+
+	fmt.Println(circ)
+	fmt.Println(explenation)
+	bmicalculator.Bmi()
 
 }
