1851d98ef574814a2cd3a1e36f0d6af04c890e67
Fenix4088 <fenix4088@gmail.com>
2022-09-10 15:04:58 +0300

    Refactor bmi package

--- bmiCalculator/bmi.go
+++ bmiCalculator/bmi.go
@@ -5,31 +5,16 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var reader = bufio.NewReader(os.Stdin)
 
 func Bmi() {
-	fmt.Println("-----BMI Calculator-----")
+	fmt.Printf("-----%v-----", appName)
 
-	 weight, _ := strconv.ParseFloat(prompt("Please enter your weight (kg): "), 32)
+	 weight, _ := strconv.ParseFloat(prompt(weightMsg), 32)
 
-	 height, _ := strconv.ParseFloat(prompt("Please enter your height (m): "), 32)
+	 height, _ := strconv.ParseFloat(prompt(heightMsg), 32)
 
 	 fmt.Printf("Your BMI is: %.2f\n", calcBmi(weight, height))
-}
-
-func prompt(question string) string {
-	fmt.Print(question)
-	result, _ := reader.ReadString('\n')
-	/*
-		-1 meens that we whant to replace all matches in a string
-		in our case all \n to empty string
-	*/
-	return strings.Replace(result, "\n", "", -1) 
-}
-
-func calcBmi(weight, height float64) float64 {
-	return weight / height * height
 }
